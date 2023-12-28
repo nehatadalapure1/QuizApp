bda46ea65eda1e353442e4a078897d7add4d62e3
Neha Tadalapure <145104870+nehatadalapure1@users.noreply.github.com>
2023-12-28 06:46:01 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+  number:1,        
+  question:'Who is known as the father of Computer?', 
+  answer:'Charles Babbage', 
+  options:[
+          'James Gosling', 
+          'Charles Babbage', 
+          'Albert Einstein', 
+          'Dennis Ritchie'
+          ]
+    }, 
+    {
+            number:2,
+            question: 'How much is a byte equal to?',
+            answer: '8 bits',
+            options: [
+          '8 bits',
+          '16 bits',
+          '32 bits',
+          '64 bits'
+          ]
+    }, 
+    {
+            number:3,
+            question: 'Which of the following is not a web browser?',
+            answer: 'Facebook',
+            options: [
+          'MOSAIC',
+          'WWW',
+          'Facebook',
+          'Netscape navigator'
+          ]
+    }, 
+    {
+            number:4,
+            question: 'What is the brain of a computer system called?',
+            answer: 'CPU',
+            options: [
+          'RAM',
+          'CPU',
+          'GPU',
+          'None of the above'
+          ]
+    }, 
+    {
+            number:5,
+            question:'Which of the following is not an output device?',
+            answer:'Scanner',
+            options:[
+          'Speakers',
+          'Monitor',
+          'Plotter',
+          'Scanner'
+          ]
+    },
+ ]
